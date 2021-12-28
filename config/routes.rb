df901d93b32b7d9f538ffdac770cd4901ec2af03
Robert Colvin <robert_colvin@hotmail.co.uk>
2021-12-28 15:54:03 +0000

    entry page started, some frontend and links to main home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "pages#entry"
   get 'profile', to: 'pages#profile'
+  get 'home', to: 'pages#home'
 
   resources :properties do
     resources :bookings, only:[:new, :create, :update, :edit, :index]
