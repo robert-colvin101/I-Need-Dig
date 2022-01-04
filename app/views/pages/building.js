98d932f61090abb404a913c73d63d928a2b75e75
Robert Colvin <robert_colvin@hotmail.co.uk>
2022-01-04 20:59:15 +0000

    contd front end

---
+++ app/views/pages/building.js
@@ -0,0 +1,15 @@
+  // 1m2 = 150
+
+//  per metre squared
+
+brickAndBlock = 219
+renderedBlock = 212
+renderedExInsulatedBlock = 238
+
+ // a = w l
+ // 2.75m average wall height
+
+function wallCost (height, length) {
+  let area = (height * length);
+  console.log(area)
+}
