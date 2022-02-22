3959ffb7282e5d6262a4c66843cde253f2c9288f
Robert Colvin <robert_colvin@hotmail.co.uk>
2022-02-22 14:17:25 +0000

    geocoding

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -14,5 +14,17 @@ export default class extends Controller {
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
     })
+    this.#addMarkersToMap()
   }
+  #addMarkersToMap() {
+  this.markersValue.forEach((marker) => {
+    new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      .addTo(this.map)
+  });
+  #fitMapToMarkers() {
+  const bounds = new mapboxgl.LngLatBounds()
+  this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+  this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
 }
+
