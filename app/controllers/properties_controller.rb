4d255cc06637958bc018279d26c33f8b4797ed9d
Robert Colvin <robert_colvin@hotmail.co.uk>
2022-02-23 11:39:26 +0000

    code

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -4,8 +4,7 @@ class PropertiesController < ApplicationController
 
   def index
     @properties = Property.all
-
-    @markers = @properties.geocoded.map do | property |
+    @markers = @properties.geocoded.map do |property|
       {
         lat: property.latitude,
         lng: property.longitude
